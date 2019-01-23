82b610766f9c26bfca92731f731656f41dd358d2
Mario Giron Martin <mariogironm@gmail.com>
2019-01-23 23:40:29 +0100

    token generator v2.2.0

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # @mariogironm/tokengen
 
 ![Versión](https://img.shields.io/badge/npm-v1.0.0-blue.svg)
+[![install size](https://packagephobia.now.sh/badge?p=@mariogironm/tokengen)](https://packagephobia.now.sh/result?p=@mariogironm/tokengen)
 
 Generador de tokens
--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-module.exports = function tiny(string) {
-    if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
-    return string.replace(/\s/g, "");
-};
+let tokengen = ({ length = 8, chars = true, numbers = true, symbols = true }) => {
+    if(!chars && !numbers && !symbols) throw new SyntaxError('Debes activar uno de los conjuntos de caracteres')
+    let resultChars = ''
+    if (chars) resultChars += 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    if (numbers) resultChars += '0123456789'
+    if (symbols) resultChars += '¡!¿?.,+{}[]/$%&'
+    let token = '';
+    for (let i = 0; i < length; i++) {
+        token += resultChars[Math.floor(Math.random() * resultChars.length)];
+    }
+    return token;
+}
+
+module.exports = tokengen
